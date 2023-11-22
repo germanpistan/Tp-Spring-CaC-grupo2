bc6e6cb5484871afeb72acfe88062597ae16eb6b
Facundo Esteban Bazán <facundobazan@icloud.com>
2023-11-22 18:51:33 -0300

    spring initializer

---
+++ src/test/java/com/ar/cac/SpringBank/SpringBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.cac.SpringBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

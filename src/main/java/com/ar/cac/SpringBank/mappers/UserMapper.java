1790d64c7d7f59affe63e26cc0f4c52b2601737d
germanpistan <anibalgermanpistan@gmail.com>
2023-11-22 20:07:03 -0300

    creado UserDto y UserMapper

---
+++ src/main/java/com/ar/cac/SpringBank/mappers/UserMapper.java
@@ -0,0 +1,34 @@
+package com.ar.cac.SpringBank.mappers;
+import com.ar.cac.SpringBank.entities.User;
+import com.ar.cac.SpringBank.entities.dtos.UserDto;
+import lombok.experimental.UtilityClass;
+
+
+
+@UtilityClass
+public class UserMapper {
+    public static User dtoToUser (UserDto dto) {
+        User user = new User();
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setEmail(dto.getEmail());
+        user.setDocument(dto.getDocument());
+        user.setPassword(dto.getPassword());
+        user.setAddress(dto.getAddress());
+        user.setBirthDate(dto.getBirthDate());
+        return user;
+    }
+
+    public static UserDto userToDto (User user) {
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        dto.setEmail(user.getEmail());
+        dto.setDocument(user.getDocument());
+        dto.setPassword(user.getPassword());
+        dto.setAddress(user.getAddress());
+        dto.setBirthDate(user.getBirthDate());
+        return dto;
+    }
+}

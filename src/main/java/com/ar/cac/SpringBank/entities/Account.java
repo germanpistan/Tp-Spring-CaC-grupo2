e0d78f77867fc8b18467126ed083b349ecee4243
nikociri1 <nicolas_ciribe@hotmail.com>
2023-11-22 20:51:06 -0300

    FIX creacion de account

--- src/main/java/com/ar/cac/SpringBank/entities/Account.java
+++ src/main/java/com/ar/cac/SpringBank/entities/Account.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity(name = "accounts")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,9 +26,9 @@ public class Account {
     private String alias;
 
     @Column(name = "amount", nullable = false)
-    private Double amount;
+    private BigDecimal amount;
 
-    //TODO
-    private String owner;
+
+    //private User owner;
 
 }

ca97012eaa7fa144048f6eefaa0dbcfb8fa5cf40
nikociri1 <nicolas_ciribe@hotmail.com>
2023-11-22 20:47:05 -0300

    creacion de usuario

--- src/main/java/com/ar/cac/SpringBank/entities/Account.java
+++ src/main/java/com/ar/cac/SpringBank/entities/Account.java
@@ -1,4 +1,32 @@
 package com.ar.cac.SpringBank.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "accounts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "accountType", nullable = false)
+    private AccessType type;
+
+    private String cbu;
+
+    private String alias;
+
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+
+    //TODO
+    private String owner;
+
 }

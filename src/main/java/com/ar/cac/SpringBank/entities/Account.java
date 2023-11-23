c05933665b14717a8ca4546579e2d5d75ff738ba
Facundo Esteban Bazán <facundobazan@icloud.com>
2023-11-22 22:28:54 -0300

    se agrega comentarios del codigo

--- src/main/java/com/ar/cac/SpringBank/entities/Account.java
+++ src/main/java/com/ar/cac/SpringBank/entities/Account.java
@@ -15,13 +15,16 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 public class Account {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // TODO: Los nombres de las tablas no se escriben en camel case, sino con guiones bajos para separar las palabras.
     @Column(name = "accountType", nullable = false)
     private AccountType type;
 
+    // TODO: El cbu y el alias deben ser unicos. También agregale la anotación en todos los atributos.
     private String cbu;
 
     private String alias;

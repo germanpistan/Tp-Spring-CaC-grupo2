2e88407d3c684e84f3041f1ce5bdaf8a9d20a591
germanpistan <anibalgermanpistan@gmail.com>
2023-11-28 20:10:37 -0300

    armando transfer

--- src/main/java/com/ar/cac/SpringBank/entities/Transfer.java
+++ src/main/java/com/ar/cac/SpringBank/entities/Transfer.java
@@ -2,9 +2,15 @@ package com.ar.cac.SpringBank.entities;
 
 import jakarta.persistence.ManyToOne;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Transfer {
     private Long id;
     private Long origin;
+    //falta target
+    private Date date;
+    private BigDecimal amount;
 
 
 
